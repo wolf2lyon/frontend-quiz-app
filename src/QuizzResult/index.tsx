2a58f3e9061a23720890beab3cb727679d5b562e
wolf2lyon <josesebastiancu@gmail.com>
2024-10-02 15:00:50 -0500

    style:Add class for device desktop

--- src/QuizzResult/index.tsx
+++ src/QuizzResult/index.tsx
@@ -18,7 +18,7 @@ const QuizzResult = ({
   console.log(toogleTheme)
   const styleDarkMode = !!toogleTheme  ? 'bg-current-dark' : '' ;
   return (
-    <div className={`flex justify-center pb-4 px-3 2xl:items-center ${styleDarkMode}`}>
+    <div className={`flex justify-center pb-4 px-3 2xl:items-center 2xl:h-screen ${styleDarkMode}`}>
       {showConfetti && onShowConfetti({})}
       <div className="w-11/12 2xl:grid 2xl:grid-flow-row 2xl:auto-rows-auto 2xl:grid-cols-2 md:gap-4 2xl:h-[85%] 2xl:w-4/5 2xl:max-w-[72.3125rem] bg-transparent">
         {onShowThemeHeader({})}
